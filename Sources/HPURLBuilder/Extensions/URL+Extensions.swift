49ad1fb6f10914e7134dc9f8e5c21f48a52e3d37
Henrik Panhans <henrik@panhans.dev>
2021-10-26 10:18:00 +0200

    Added variants of the build method with prefix and suffix parameters

--- Sources/HPURLBuilder/Extensions/URL+Extensions.swift
+++ Sources/HPURLBuilder/Extensions/URL+Extensions.swift
@@ -2,25 +2,55 @@ import Foundation
 
 public extension URL {
 
-	/// Attempts to construct a `URL` instance given the modifiers in the `block` closure. The default scheme will be set to `https`
-	/// - Parameter block: a closure that provides modifier for `URLComponents`
+	/// Attempts to construct a `URL` instance given the modifiers in the `modifierClosure` block. The default scheme will be set to `https`
+	/// - Parameters:
+	///     - prefixModifiers: an array of optional modifiers that will be executed before the elements of the `block` closure
+	///     - suffixModifiers: an array of optional modifiers that will be executed after the elements of the `block` closure
+	/// 	- modifierClosure: a closure that provides modifier for `URLComponents`
 	/// - Returns: a `URL` instance if the modified components' `url` property is not nil
-	static func build(@URLComponentsBuilder block: () -> [URLComponentsModifier?]) -> URL? {
+	static func build(
+		prefixModifiers: [URLComponentsModifier?],
+		suffixModifiers: [URLComponentsModifier?],
+		@URLComponentsBuilder modifierClosure: () -> [URLComponentsModifier?]
+	) -> URL? {
 		var components = URLComponents()
 		components.scheme = "https"
-		let blocks = block().compactMap { $0 }
+		let blocks = (prefixModifiers + modifierClosure() + suffixModifiers).compactMap { $0 }
 		blocks.forEach { $0.modifyURLComponents(&components) }
 		return components.url
 	}
 
-	/// Attempts to construct a `URL` instance given the modifiers in the `block` closure. The default scheme will be set to `https`
-	/// - Parameter block: a closure that provides modifier for `URLComponents`
+	/// Attempts to construct a `URL` instance given the modifiers in the `modifierClosure` block. The default scheme will be set to `https`
+	/// - Parameters:
+	/// 	- modifierClosure: a closure that provides modifier for `URLComponents`
 	/// - Returns: a `URL` instance if the modified components' `url` property is not nil
-	static func buildThrowing(@URLComponentsBuilder block: () -> [URLComponentsModifier?]) throws -> URL {
-		guard let url = build(block: block) else {
+	static func build(@URLComponentsBuilder modifierClosure: () -> [URLComponentsModifier?]) -> URL? {
+		build(prefixModifiers: [], suffixModifiers: [], modifierClosure: modifierClosure)
+	}
+
+	/// Attempts to construct a `URL` instance given the modifiers in the `modifierClosure` block. The default scheme will be set to `https`
+	/// - Parameters:
+	///     - prefixModifiers: an array of optional modifiers that will be executed before the elements of the `block` closure
+	///     - suffixModifiers: an array of optional modifiers that will be executed after the elements of the `block` closure
+	/// 	- modifierClosure: a closure that provides modifier for `URLComponents`
+	/// - Returns: a `URL` instance if the modified components' `url` property is not nil
+	static func buildThrowing(
+		prefixModifiers: [URLComponentsModifier?],
+		suffixModifiers: [URLComponentsModifier?],
+		@URLComponentsBuilder modifierClosure: () -> [URLComponentsModifier?]
+	) throws -> URL {
+		guard let url = build(prefixModifiers: prefixModifiers, suffixModifiers: suffixModifiers, modifierClosure: modifierClosure) else {
 			throw NSError.urlBuilderFailed
 		}
 		return url
+	}
+
+	/// Attempts to construct a `URL` instance given the modifiers in the `modifierClosure` block. The default scheme will be set to `https`
+	/// - Parameters:
+	/// 	- modifierClosure: a closure that provides modifier for `URLComponents`
+	/// - Returns: a `URL` instance if the modified components' `url` property is not nil
+	static func buildThrowing(@URLComponentsBuilder modifierClosure: () -> [URLComponentsModifier?]) throws -> URL {
+		try buildThrowing(prefixModifiers: [], suffixModifiers: [], modifierClosure: modifierClosure)
 	}
 
 }
